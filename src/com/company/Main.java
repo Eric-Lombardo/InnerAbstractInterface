d640ba4937539fb85a274995657f965a1683030c
Eric Lombardo <elombardo@bkom.com>
2021-10-18 20:42:36 -0400

    Removed test code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Player newPlayer = new Player("sparrow", 21, 99);
-        System.out.println(newPlayer.toString());
+        // write code here
     }
 }
